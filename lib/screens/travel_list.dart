8127a26a2f8177b95d9451f4b9e48ab32498d1b5
Minuli-Lavanya <minulilavanya@gmail.com>
2021-07-09 11:48:43 +0530

    add travel list dart

--- lib/screens/travel_list.dart
+++ lib/screens/travel_list.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:travel_app/models/TravelModel.dart';
+import 'package:travel_app/provider/travel_data_provider.dart';
 
 class travel_list extends StatefulWidget {
   const travel_list({Key? key}) : super(key: key);
@@ -9,25 +12,69 @@ class travel_list extends StatefulWidget {
 
 class _travel_listState extends State<travel_list> {
   @override
+  void initState() {
+    // TODO: implement initState
+    final mdl = Provider.of<TravelDataProvider>(context, listen: false);
+    mdl.loadTravel();
+  }
+  
+  @override
   Widget build(BuildContext context) {
+    final mdl = Provider.of<TravelDataProvider>(context);
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child:Text("Hello world"),
+      body: ListView.builder(
+          padding: EdgeInsets.all(20),
+          itemCount: mdl.travelList.length,
+          itemBuilder: (context, index){
+          TravelModel Travel = mdl.travelList[index];
+          return TravelViewCard(
+              name: Travel.name,
+              shortDetail: Travel.shortDetail,
+              image: Travel.image
+          );
+        }
       ),
     );
   }
 }
 
 class TravelViewCard extends StatelessWidget {
+  final String image;
+  final String name;
+  final String shortDetail;
+
+  TravelViewCard(
+      {required this.shortDetail, required this.image, required this.name});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container()
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(image), fit: BoxFit.cover)),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              name,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color.fromRGBO(54, 54, 54, 1)),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(shortDetail, style: TextStyle(color: Color.fromRGBO(141, 144, 145, 1)),)
           ],
         ),
       ),
