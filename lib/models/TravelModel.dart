8d11ef73637a506b3b9d29d11593a52982974f68
Minuli-Lavanya <minulilavanya@gmail.com>
2021-07-11 09:25:15 +0530

    changed travel model

--- lib/models/TravelModel.dart
+++ lib/models/TravelModel.dart
@@ -2,12 +2,14 @@ class TravelModel {
   final String name;
   final String image;
   final String shortDetail;
+  final String description;
 
-  TravelModel ({required this.name, required this.image, required this.shortDetail});
+  TravelModel ({required this.name, required this.image, required this.shortDetail, required this.description});
 
   factory TravelModel.fromJson(Map<String, dynamic> json) => TravelModel(
       name: json['name'],
       image: json['image'],
-      shortDetail: json['short-detail']
+      shortDetail: json['short-detail'],
+      description: json['description']
   );
 }
